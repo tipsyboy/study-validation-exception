1d3d175e3adeb0bacdf134b7814a45fa382adaec
TipsyBoy <gudah13@naver.com>
2023-09-07 00:22:45 +0900

    Initial commit

---
+++ src/test/java/study/validationexception/validationexception/ValidationExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package study.validationexception.validationexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
